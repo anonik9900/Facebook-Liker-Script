5fa8e2931c17c930ccae4c9eebd2df997cf9533d
DeVeL_AnOnIk <impieri.nicholas@gmail.com>
2018-03-26 17:38:56 +0200

    Add files via upload

---
+++ Facebook Page Liker/Facebook Page Liker.js
@@ -0,0 +1,4 @@
+
+              alert("Visisita la nostra pagina facebook");
+               window.open("http://facebook.com/your-facebook-page-here");
+              alert("Grazie per aver messo Like :)");
